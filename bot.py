6ff8f6df5465c9df309da430e09d7c635a7e9203
temas777 <91739590+temas777@users.noreply.github.com>
2024-12-08 00:04:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import random
 
 # Ваш токен
-TOKEN = "ВАШ_ТОКЕН"
+TOKEN = "7792709244:AAFkwlX6248F3XaIAiB1KnFMMfYyKuowuXQ"
 
 # Список мотивационных сообщений
 messages = [
